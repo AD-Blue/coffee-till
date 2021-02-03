16a709bcca9af1335a909ec42fa777a9c0f6b748
The-Real-Guy <aasdimaranan@gmail.com>
2021-02-02 23:56:43 -0700

    Created basic layout of size-selector bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import Menu from './Components/Menu';
-import { Flex } from "@chakra-ui/react";
+import { Flex, Box } from "@chakra-ui/react";
 import ItemList from './Components/Item/ItemList';
+import SizeBar from './Components/SizeBar';
 
 function App() {
   return (
     <Flex className="App" flexDirection="row" h="100%">
       <ItemList />
+      <SizeBar />
       <Menu />
     </Flex>
   );
---
+++ client/src/Components/SizeBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Flex, Button, Spacer } from "@chakra-ui/react";
+
+export default function SizeBar() {
+    return (
+        <Flex flexDirection="column" h="100%" bg="green" alignContent="space-between">
+            <Spacer />
+            <Button>
+                S
+            </Button>
+            <Spacer />
+            <Button>
+                M
+            </Button>
+            <Spacer />
+            <Button>
+                L
+            </Button>
+            <Spacer />
+        </Flex>
+    )
+}
