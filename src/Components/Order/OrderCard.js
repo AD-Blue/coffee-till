6fa21928eb3c47728a6f086f08391927fcc5014d
The-Real-Guy <aasdimaranan@gmail.com>
2021-04-26 14:42:10 -0600

    Added basic refund functionality (just deletes the order for now

--- src/Components/Order/OrderCard.js
+++ src/Components/Order/OrderCard.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import { Flex, Text, ListItem, Heading, UnorderedList } from '@chakra-ui/react';
+import React, { useContext } from 'react';
+import { Flex, Text, ListItem, Heading, UnorderedList, Button } from '@chakra-ui/react';
 import { numberWithCommas } from '../../utils/format';
 import OrderItem from './OrderItem';
+import { GlobalContext } from '../../Context/GlobalState';
 
 export default function OrderCard({order}) {
+    const { removeOrder } = useContext(GlobalContext);
+
+    const handleClick = () => {
+        removeOrder(order._id)
+    }
+
     return (
         <ListItem w='350px'>
             <Flex border='2px' borderColor='brand.400' direction='column' ml='2%' mr='2%' p='2%'>
@@ -17,6 +24,7 @@ export default function OrderCard({order}) {
                     )}
                 </UnorderedList>
                 <Text textAlign='right' mt='10%' fontSize='24px' color='brand.400'>Total: ${numberWithCommas(order.total.toFixed(2))}</Text>
+                <Button colorScheme='teal' onClick={handleClick}>Refund</Button>
             </Flex>
         </ListItem>
     )
